405a6e47380b11d690fae655434c17bff4c3ddcf
iclicker <johndoe00.dj@gmail.com>
2017-07-12 00:56:00 +0900

    Update img_loader.py

--- img_loader.py
+++ img_loader.py
@@ -6,6 +6,7 @@ import numpy as np
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import dtypes
 import os.path
+import skimage.io as io
 
 dataset_path      = "train/"
 train_labels_file = "train_master.tsv"
@@ -13,14 +14,16 @@ test_path = "test/"
 test_laabels_file = "sample_submit.csv"
 logdir = '/tmp/uniqlo_2/'
 
-IMAGE_HEIGHT  = 133
-IMAGE_WIDTH   = 133
+IMAGE_HEIGHT  = 128
+IMAGE_WIDTH   = 128
 NUM_CHANNELS  = 3
 BATCH_SIZE = 100
 TEST_BATCH_SIZE = 100
+NUM_CLASSES = 24
 
 def ratio_fn():
     ratio = list(np.ones(NUM_CLASSES)*(1/NUM_CLASSES))
+    #ratio = tf.convert_to_tensor(ratio, dtype=dtypes.float32)
     return ratio
 
 def encode_label(label):
@@ -40,27 +43,48 @@ def file_loader():
     train_filepaths, train_labels = read_label_file(train_labels_file)
     num_files = len(train_filepaths)
     
-    # transform relative path into full path
-    train_filepaths = [ dataset_path + fp for fp in train_filepaths]
-    
-    # convert string into tensors
-    train_filepaths = ops.convert_to_tensor(train_filepaths, dtype=dtypes.string)
-    train_labels = ops.convert_to_tensor(train_labels, dtype=dtypes.int32)
-    
     if os.path.exists(logdir+"partition.csv"):
         partitions = np.array(pd.read_csv(logdir+"partition.csv",header=None))
         partitions = list(partitions.ravel())
     else:
         # create a partition vector
         partitions = [0] * num_files
-        partitions[:int(0.3*num_files)] = [1] * int(0.3*num_files)
+        partitions[:int(0.25*num_files)] = [1] * int(0.25*num_files)
         random.shuffle(partitions)
         np.savetxt(logdir+"/partition.csv", partitions, delimiter=",", fmt='%s')
     
+    # transform relative path into full path
+    train_filepaths = [ dataset_path + fp for fp in train_filepaths]
     
     # partition our data into a test and train set according to our partition vector
-    train_images, eval_images = tf.dynamic_partition(train_filepaths, partitions, 2)
-    train_labels, eval_labels = tf.dynamic_partition(train_labels, partitions, 2)
+    #train_images, eval_images = tf.dynamic_partition(train_filepaths, partitions, 2)
+    #train_labels, eval_labels = tf.dynamic_partition(train_labels, partitions, 2)
+    
+    
+   
+    # equalize categorical proportions
+    all_files = pd.DataFrame([train_filepaths, train_labels, partitions]).transpose()
+    all_files.columns = (['file','label','partition'])
+    
+    eval_images = list(all_files.file[all_files.partition==1])
+    eval_labels = list(all_files.label[all_files.partition==1])
+    
+    all_files = all_files[all_files.partition == 0]
+    all_files.drop('partition', axis=1, inplace=True)
+    
+    threshold = max(all_files.label.value_counts())
+    for i in range(NUM_CLASSES):
+        while len(all_files[all_files.label == i]) < (threshold/2):
+            all_files = all_files.append(all_files[all_files.label == i])
+    
+    train_filepaths = list(all_files.file)
+    train_labels = list(all_files.label)
+    
+    # convert string into tensors
+    train_images = ops.convert_to_tensor(train_filepaths, dtype=dtypes.string)
+    train_labels = ops.convert_to_tensor(train_labels, dtype=dtypes.int32)
+    eval_images = ops.convert_to_tensor(eval_images, dtype=dtypes.string)
+    eval_labels = ops.convert_to_tensor(eval_labels, dtype=dtypes.int32)
     
     return train_images, train_labels, eval_images, eval_labels
 
@@ -74,14 +98,21 @@ def input():
     # process path and string tensor into an image and a label
     file_content = tf.read_file(train_input_queue[0])
     train_image = tf.image.decode_jpeg(file_content, channels=NUM_CHANNELS)
+    train_image = tf.random_crop(train_image, [600, 600, 3])
     train_image = tf.image.resize_images(train_image, [IMAGE_HEIGHT, IMAGE_WIDTH])
-    train_image = tf.image.per_image_standardization(train_image)
-    #train_image = tf.cast(train_image, tf.float32)
+    #train_image = tf.image.per_image_standardization(train_image)
+    train_image = tf.cast(train_image, tf.float32)/255
+    
+    # image flipping, shearing
+    train_image = tf.image.random_flip_left_right(train_image)
+    train_image = tf.image.random_flip_up_down(train_image)
+    
     train_label = train_input_queue[1]
+    
+
     
     # define tensor shape
     train_image.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
-    
     
     '''
     # collect batches of images before processing
@@ -91,7 +122,6 @@ def input():
                                     #,num_threads=1
                                     )
     '''
-    # collect stratified sample and create batches
     target_probs = ratio_fn()
     [train_image_batch], train_label_batch = tf.contrib.training.stratified_sample(
                                     [train_image],
@@ -161,8 +191,8 @@ def input_test():
     return test_image_batch, test_label_batch, num_files
 
 ###################################################################################
-'''
 
+'''
 img, label = input()
 with tf.Session() as sess:
   
@@ -174,12 +204,19 @@ with tf.Session() as sess:
   threads = tf.train.start_queue_runners(coord=coord)
 
   print("from the train set:")
-  #img = sess.run(img[1])/5
-  #io.imshow(img)
+  img = sess.run(img[1])
+  io.imshow(img)
   print(sess.run(label))
-  #example = sess.run(test_image_batch[2])/255
-  #io.imshow(example)
   
+  
+  a=pd.DataFrame()
+  for i in range(100):
+        print(i)
+        a = a.append(pd.DataFrame(label.eval()))
+        a.hist(bins=24)
+  
+  example = sess.run(test_image_batch[1])/255
+  io.imshow(example)
   # stop our queue threads and properly close the session
   coord.request_stop()
   coord.join(threads)
